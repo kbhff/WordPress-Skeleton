6753e3fa70975c917db09fa4b99c67de1bfddb51
Jens Balvig <jens@balvig.com>
2014-01-27 15:13:01 +0100

    update sample to work with mamp url

--- local-config-sample.php
+++ local-config-sample.php
@@ -6,7 +6,12 @@ In it, you *must* include the four main database defines
 You may include other settings here that you only want enabled on your local development checkouts
 */
 
-define( 'DB_NAME', 'local_db_name' );
-define( 'DB_USER', 'local_db_user' );
-define( 'DB_PASSWORD', 'local_db_password' );
+define( 'DB_NAME', 'kbhff_dk_db' );
+define( 'DB_USER', 'root' );
+define( 'DB_PASSWORD', 'root' );
 define( 'DB_HOST', 'localhost' ); // Probably 'localhost'
+
+// For local development
+define('WP_HOME', 'http://localhost:8888');
+define('WP_SITEURL', 'http://localhost:8888');
+define('WP_DEBUG', true);
